bc74582c6bfdc696bc0e192bccfd47d9f3053ecf
Kaleb Cooper <thekalebcooper@gmail.com>
2020-12-04 15:09:45 -0600

    initial test commit

---
+++ Sources/CitadelCore/CitadelHelper.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kaleb Cooper on 12/4/20.
+//
+
+import Foundation
+
+public class CitadelHelper {
+    public static let constant: String = "Constant string"
+}
